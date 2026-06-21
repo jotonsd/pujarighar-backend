b125764a60583b891100bfd27d0fea2acac98b25
Joton Sutradhar <joton@flyghuri.com>
2026-06-21 22:22:54 +0600

    update

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -6,14 +6,17 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.postgresql',
+        'ENGINE':   'django.db.backends.mysql',
         'NAME':     config('DB_NAME',     default='pujarighar'),
-        'USER':     config('DB_USER',     default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default='postgres'),
+        'USER':     config('DB_USER',     default='root'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST':     config('DB_HOST',     default='localhost'),
-        'PORT':     config('DB_PORT',     default='5432'),
+        'PORT':     config('DB_PORT',     default='3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://dev.pujarighar.com/']
 CORS_ALLOW_CREDENTIALS = True
