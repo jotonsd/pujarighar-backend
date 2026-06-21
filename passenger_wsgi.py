c5dfa3b337e08f3ab38597d0bad334b0473c8c64
Joton Sutradhar <joton@flyghuri.com>
2026-06-21 22:26:40 +0600

    update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,7 +4,7 @@ import os
 # Add project root to the path
 sys.path.insert(0, os.path.dirname(__file__))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
