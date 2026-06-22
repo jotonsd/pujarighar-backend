37d6c2f7c9629ffb00581f3ea1abbc7edbb61dbc
Joton Sutradhar <joton@flyghuri.com>
2026-06-22 09:52:26 +0600

    update setting

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,8 @@
 import os
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
+SETTINGS_MODULE = 'core.settings.prod' if config('ENVIRONMENT', default='development') == 'production' else 'core.settings.dev'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', SETTINGS_MODULE)
 application = get_wsgi_application()
